3682bb6b7f2fe484543c070ad0ebceb21656717e
Matthew McGrath <rubywep@hotmail.com>
2021-10-21 11:54:35 -0700

    Fix bug preventing background image and particles from blending (was just missing background color on particle canvas.

--- Particle Effects on Mousemove/app.js
+++ Particle Effects on Mousemove/app.js
@@ -10,118 +10,119 @@ particlesJS.load('particles-js', 'particles.json', function() {
 */
 
 /* Otherwise just put the config content (json): */
-let maxParticles = 400;
-let color = "#000";
+let maxParticles = 800;
+let color = '#000';
 
-particlesJS('particles-js',
+particlesJS(
+  'particles-js',
 
   {
-    "particles": {
-      "number": {
-        "value": maxParticles,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": color // "#000"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
+    particles: {
+      number: {
+        value: maxParticles,
+        density: {
+          enable: true,
+          value_area: 400,
         },
-        "polygon": {
-          "nb_sides": 5
+      },
+      color: {
+        value: color, // "#000"
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 0,
+          color: '#000000',
         },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: 'img/github.svg',
+          width: 100,
+          height: 100,
+        },
       },
-      "opacity": {
-        "value": 1, // 1
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
+      opacity: {
+        value: 1, // 1
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
       },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
       },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#000",
-        "opacity": 0.4,
-        "width": 1
+      line_linked: {
+        enable: false,
+        distance: 150,
+        color: '#000',
+        opacity: 0.4,
+        width: 1,
       },
-      "move": {
-        "enable": true,
-        "speed": 5,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
+      move: {
+        enable: true,
+        speed: 5,
+        direction: 'none',
+        random: false,
+        straight: false,
+        out_mode: 'out',
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
+    interactivity: {
+      detect_on: 'canvas',
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'bubble',
         },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
+        onclick: {
+          enable: true,
+          mode: 'push',
         },
-        "resize": true
+        resize: true,
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
         },
-        "bubble": {
-          "distance": 400,
-          "size": 100,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
+        bubble: {
+          distance: 400,
+          size: 50,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
         },
-        "repulse": {
-          "distance": 200
+        repulse: {
+          distance: 200,
         },
-        "push": {
-          "particles_nb": 4
+        push: {
+          particles_nb: 4,
         },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+        remove: {
+          particles_nb: 2,
+        },
+      },
     },
-    "retina_detect": true,
+    retina_detect: true,
     // "config_demo": {
     //   "hide_card": false,
     //   "background_color": "#b61924",
@@ -131,5 +132,4 @@ particlesJS('particles-js',
     //   "background_size": "cover"
     // }
   }
-
 );
